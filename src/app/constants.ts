34da01cc37866605cc0ab83d48037bef2b756492
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-29 19:55:26 +0300

    constants

---
+++ src/app/constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+    public static readonly YT_KEY: string = 'AIzaSyAAPTMdrlg8_T9R7vT61uI2U0whoCpfFK8';
+    public static readonly VIDEO_URL: string = 'https://www.youtube.com/embed/';
+}
