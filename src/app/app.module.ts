b2c20348279f1a5cf4fb921be36c5432ce8fb3a7
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-30 00:00:32 +0300

    adjust adaptivity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 
 import {AppComponent} from './app.component';
 import {ContentListComponent} from './content-list/content-list.component';
 import {ListItemComponent} from './content-list/components/list-item/list-item.component';
+import {InlineSearchComponent} from './components/inline-search/inline-search.component';
 
 @NgModule({
-    declarations: [AppComponent, ContentListComponent, ListItemComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    declarations: [AppComponent, ContentListComponent, ListItemComponent, InlineSearchComponent],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
