866a89fe93fa535651fa054e5c12960a68b70055
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-29 15:18:30 +0300

    prepare project

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    arrowParens: 'always',
+    htmlWhitespaceSensitivity: 'strict',
+    // endOfLine: 'crlf'
+};
