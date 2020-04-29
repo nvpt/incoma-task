60cf0a6ac9cefb24dc0fb9d2fd63b27486800b27
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-29 16:59:07 +0300

    list draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { ListItemComponent } from './content-list/components/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
