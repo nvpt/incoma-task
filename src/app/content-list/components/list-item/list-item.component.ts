2f06c48eb9c220c7a1df1815999c4b0585c463bb
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-29 19:46:00 +0300

    list-item.component in progress

--- src/app/content-list/components/list-item/list-item.component.ts
+++ src/app/content-list/components/list-item/list-item.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+
+import {Constants} from '../../../constants';
+import {VideoI} from '../../../services/youtube.service';
 
 @Component({
-  selector: 'app-list-item',
-  templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.scss']
+    selector: 'app-list-item',
+    templateUrl: './list-item.component.html',
+    styleUrls: ['./list-item.component.scss']
 })
 export class ListItemComponent implements OnInit {
+    @Input() video?: VideoI;
+    constants = Constants;
+    selected: boolean = false;
 
-  constructor() { }
+    constructor(public sanitizer: DomSanitizer) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    toggle(): void {
+        this.selected = !this.selected;
+    }
 }
