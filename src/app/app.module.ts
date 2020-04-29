33c196075829fb234ce6f7d4f0d7f4d274c64135
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-29 22:40:20 +0300

    adjust control panel view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContentListComponent } from './content-list/content-list.component';
-import { ListItemComponent } from './content-list/components/list-item/list-item.component';
+import {AppRoutingModule} from './app-routing.module';
+
+import {AppComponent} from './app.component';
+import {ContentListComponent} from './content-list/content-list.component';
+import {ListItemComponent} from './content-list/components/list-item/list-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContentListComponent,
-    ListItemComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, ContentListComponent, ListItemComponent],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
