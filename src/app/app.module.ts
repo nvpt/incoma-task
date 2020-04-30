8593fc363e349af284f54fc07e76879f6b39cd83
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-30 22:49:42 +0300

    infinite-scroll directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {LoaderComponent} from './components/loader/loader.component';
 import {FilterItemsPipe} from './content-list/pipes/filter-items.pipe';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { InfiniteScrollDirective } from './directives/infinite-scroll.directive';
 
 @NgModule({
     declarations: [
@@ -22,7 +23,8 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
         ListItemComponent,
         InlineSearchComponent,
         LoaderComponent,
-        FilterItemsPipe
+        FilterItemsPipe,
+        InfiniteScrollDirective
     ],
     imports: [
         BrowserModule,
