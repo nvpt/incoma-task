091dd8c3325af2beb7b16ab5fd473d0a9f4e2b58
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-30 10:02:56 +0300

    added loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import {AppComponent} from './app.component';
 import {ContentListComponent} from './content-list/content-list.component';
 import {ListItemComponent} from './content-list/components/list-item/list-item.component';
 import {InlineSearchComponent} from './components/inline-search/inline-search.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-    declarations: [AppComponent, ContentListComponent, ListItemComponent, InlineSearchComponent],
+    declarations: [AppComponent, ContentListComponent, ListItemComponent, InlineSearchComponent, LoaderComponent],
     imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {VideoI, YoutubeService} from '../services/youtube.service';
 
 @Component({
-  selector: 'app-content-list',
-  templateUrl: './content-list.component.html',
-  styleUrls: ['./content-list.component.scss']
+    selector: 'app-content-list',
+    templateUrl: './content-list.component.html',
+    styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent implements OnInit {
+    videos: VideoI[] = [];
+    filterValue: string = '';
 
-  constructor() { }
+    constructor(private youtube: YoutubeService, ) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        // this.getVideoList();
+    }
 
+    getVideoList() {
+        this.youtube.getVideoList().subscribe((res) => {
+            console.log('18 >>> res: ', res);
+            this.videos = res.items;
+        });
+    }
 }
