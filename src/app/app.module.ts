f7e7193d887e70e3cd79e7b20810292034c7f103
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-05-01 14:17:05 +0300

    connect shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,40 +3,23 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
-import {YouTubePlayerModule} from '@angular/youtube-player';
+
+
+import {SharedModule} from './shared/shared.module';
 
 import {AppComponent} from './app.component';
-import {TopRatePageComponent} from './top-rate-page/top-rate-page.component';
-import {ListItemComponent} from './top-rate-page/components/list-item/list-item.component';
-import {InlineSearchComponent} from './components/inline-search/inline-search.component';
-import {LoaderComponent} from './components/loader/loader.component';
-import {FilterItemsPipe} from './top-rate-page/pipes/filter-items.pipe';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {InfiniteScrollDirective} from './directives/infinite-scroll.directive';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     declarations: [
         AppComponent,
-        TopRatePageComponent,
-        ListItemComponent,
-        InlineSearchComponent,
-        LoaderComponent,
-        FilterItemsPipe,
-        InfiniteScrollDirective
+
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
         FormsModule,
-        BrowserAnimationsModule,
-        YouTubePlayerModule,
-        MatProgressSpinnerModule,
-        MatCheckboxModule,
-        MatButtonModule
+        SharedModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,55 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {YouTubePlayerModule} from '@angular/youtube-player';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+/*ANGULAR MATERIAL*/
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+
+/*COMPONENTS*/
+import {TopRatePageComponent} from '../top-rate-page/top-rate-page.component';
+import {ListItemComponent} from '../top-rate-page/components/list-item/list-item.component';
+import {InlineSearchComponent} from '../components/inline-search/inline-search.component';
+import {LoaderComponent} from '../components/loader/loader.component';
+
+/*PIPES*/
+import {FilterItemsPipe} from '../top-rate-page/pipes/filter-items.pipe';
+
+/*DIRECTIVES*/
+import {InfiniteScrollDirective} from '../directives/infinite-scroll.directive';
+
+@NgModule({
+    declarations: [
+        TopRatePageComponent,
+        ListItemComponent,
+        InlineSearchComponent,
+        LoaderComponent,
+        FilterItemsPipe,
+        InfiniteScrollDirective
+    ],
+    imports: [
+        CommonModule,
+        MatProgressSpinnerModule,
+        MatCheckboxModule,
+        MatButtonModule,
+        YouTubePlayerModule,
+        BrowserAnimationsModule
+    ],
+    exports: [
+        CommonModule,
+        MatProgressSpinnerModule,
+        MatCheckboxModule,
+        MatButtonModule,
+        YouTubePlayerModule,
+        BrowserAnimationsModule,
+        TopRatePageComponent,
+        ListItemComponent,
+        InlineSearchComponent,
+        LoaderComponent,
+        FilterItemsPipe,
+        InfiniteScrollDirective
+    ]
+})
+export class SharedModule {}
